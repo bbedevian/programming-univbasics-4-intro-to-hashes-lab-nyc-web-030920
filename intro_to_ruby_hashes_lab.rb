e725e3d0d4f721d3779ba870046ba53f179c01d1
Brett Bedevian <brettbedevian@gmail.com>
2020-02-12 16:00:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,12 +19,12 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  hash1 = {key: value}
+  hash1 = {"#{key}": value}
   hash1
 end
 
 def read_from_hash(hash, key)
- hash[:key]
+ hash[:"#{key}"]
 end
 
 def update_counting_hash(hash, key)
